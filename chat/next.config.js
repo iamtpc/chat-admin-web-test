e128138c9b536e31828128ab120af9fa635fa3bc
iamtpc <62536247+TPC-Light@users.noreply.github.com>
2023-06-16 19:13:35 +0800

    Add files via upload

---
+++ chat/next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    }); // 针对 SVG 的处理规则
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
